e937eecbd6b6e57048129cc6483b3d4c8905483a
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-04-01 21:40:14 +0700

    Initial Commit

---
+++ Appulse WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Appulse WatchKit Extension
+//
+//  Created by Dmitry Matveev on 01/04/2017.
+//  Copyright © 2017 Dmitry Matveev. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Appulse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Appulse
+//
+//  Created by Dmitry Matveev on 01/04/2017.
+//  Copyright © 2017 Dmitry Matveev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
