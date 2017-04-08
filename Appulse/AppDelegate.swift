378fa843132d8a47b63ba424334ddbe75da3f7ab
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-04-08 06:40:20 +0300

    add crash reporter

--- Appulse/AppDelegate.swift
+++ Appulse/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let config: RollbarConfiguration = RollbarConfiguration()
+        config.environment = "production"
+
+        Rollbar.initWithAccessToken("YOUR ACCESS TOKEN", configuration: config)
+
         return true
     }
 
